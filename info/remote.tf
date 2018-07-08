87bcd9b9a88016d93b45a59130c64fd99f8265a6
psharmadevops <psharmadevops@gmail.com>
2018-07-08 09:23:48 +0530

    files will be passed by ansible

--- info/remote.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "assgnmt-terraform-states"
-    region = "us-east-2"
-    encrypt="true"
-    key="terraform.tfstate"
-#    lock_table = "terraform-remote-states-locking"
-  }
-}
